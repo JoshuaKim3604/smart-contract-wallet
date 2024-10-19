565c4a0bb3146c543615ba64e0db98125e5d48d4
Joshua <hackingolivetr@gmail.com>
2024-10-19 18:49:01 +0900

    feat: add self call checker

---
+++ contracts/facets/utils/SelfCallChecker.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.27;
+
+abstract contract SelfCallChecker {
+
+    error CallerNotSelf();
+
+    modifier enforceSelfCall() {
+        require(msg.sender == address(this), CallerNotSelf());
+        _;
+    }
+
+}
